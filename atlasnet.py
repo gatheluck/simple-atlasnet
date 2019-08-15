013e509b3816827677c481e418dffa8a8229a2d1
Yoshihiro Fukuhara <gatheluck+tech@gmail.com>
2019-08-15 23:51:42 +0900

    update atlasnet

--- atlasnet.py
+++ atlasnet.py
@@ -1,8 +1,7 @@
 __all__ = [
 	'PointGenCon',
-  'PointGenCon_SkipConection',
-	'SVR_AtlasNet_SPHERE',
-  'SVR_AtlasNet_SPHERE_Plus',
+  'PointGenSkipconnectedDecoder',
+	'AtlasNetSingle',
 ]
 
 import torch
@@ -11,73 +10,74 @@ import torch.nn.functional as F
 from torchvision import models
 
 class PointGenDecoder(nn.Module):
-  def __init__(self, 
-               latent_size):
-    
-    self.bottleneck_size = bottleneck_size
-    super(PointGenCon_SkipConection, self).__init__()
-    self.conv1 = torch.nn.Conv1d((self.bottleneck_size//1)+3, self.bottleneck_size//1, kernel_size=1)
-    self.conv2 = torch.nn.Conv1d((self.bottleneck_size//1)+3, self.bottleneck_size//2, kernel_size=1)
-    self.conv3 = torch.nn.Conv1d((self.bottleneck_size//2)+3, self.bottleneck_size//4, kernel_size=1)
-    self.conv4 = torch.nn.Conv1d((self.bottleneck_size//4)+3, 3, kernel_size=1)
-    
-    self.th = nn.Tanh()
-    self.bn1 = torch.nn.BatchNorm1d(self.bottleneck_size//1)
-    self.bn2 = torch.nn.BatchNorm1d(self.bottleneck_size//2)
-    self.bn3 = torch.nn.BatchNorm1d(self.bottleneck_size//4)
-    
-  def forward(self, z, grid):
-      # z    : (#batch, #bottleneck_size)
-      # grid : (#batch, 3, #points)
-
-      x = x[:,:,None].repeat(1,1,grid.size(2)) # x : (#batch, #bottleneck_size, #points)
-
-      x = torch.cat((x, grid), 1) 
-      x = F.relu(self.bn1(self.conv1(x)))
-
-      x = torch.cat((x, grid), 1)
-      x = F.relu(self.bn2(self.conv2(x)))
-
-      x = torch.cat((x, grid), 1)
-      x = F.relu(self.bn3(self.conv3(x)))
-
-      x = torch.cat((x, grid), 1)
-      x = self.th(self.conv4(x))
-      return x
-
-class PointGenDecoderSkipconnected(nn.Module):
 	def __init__(self, latent_size):
     
-    self.bottleneck_size = bottleneck_size
-    super(PointGenCon_SkipConection, self).__init__()
-    self.conv1 = torch.nn.Conv1d((self.bottleneck_size//1)+3, self.bottleneck_size//1, kernel_size=1)
-    self.conv2 = torch.nn.Conv1d((self.bottleneck_size//1)+3, self.bottleneck_size//2, kernel_size=1)
-    self.conv3 = torch.nn.Conv1d((self.bottleneck_size//2)+3, self.bottleneck_size//4, kernel_size=1)
-    self.conv4 = torch.nn.Conv1d((self.bottleneck_size//4)+3, 3, kernel_size=1)
+    self.latent_size = latent_size
+    super(PointGenDecoder, self).__init__()
+    self.conv1 = torch.nn.Conv1d(self.latent_size//1, self.latent_size//1, kernel_size=1)
+    self.conv2 = torch.nn.Conv1d(self.latent_size//1, self.latent_size//2, kernel_size=1)
+    self.conv3 = torch.nn.Conv1d(self.latent_size//2, self.latent_size//4, kernel_size=1)
+    self.conv4 = torch.nn.Conv1d(self.latent_size//4, 3, kernel_size=1)
     
     self.th = nn.Tanh()
-    self.bn1 = torch.nn.BatchNorm1d(self.bottleneck_size//1)
-    self.bn2 = torch.nn.BatchNorm1d(self.bottleneck_size//2)
-    self.bn3 = torch.nn.BatchNorm1d(self.bottleneck_size//4)
+    self.bn1 = torch.nn.BatchNorm1d(self.latent_size//1)
+    self.bn2 = torch.nn.BatchNorm1d(self.latent_size//2)
+    self.bn3 = torch.nn.BatchNorm1d(self.latent_size//4)
     
-  def forward(self, z, grid):
-      # z    : (#batch, #bottleneck_size)
-      # grid : (#batch, 3, #points)
+  def forward(self, z, verts):
+      # z     : (#batch, #latent_size)
+      # verts : (#batch, 3, #vertex)
 
-      x = x[:,:,None].repeat(1,1,grid.size(2)) # x : (#batch, #bottleneck_size, #points)
+			num_verts = verts.size(2)
+      z = z[:,:,None].repeat(1,1,num_verts) # z: (#batch, #latent_size, #vertex)
 
-      x = torch.cat((x, grid), 1) 
-      x = F.relu(self.bn1(self.conv1(x)))
+      y = torch.cat((z, verts), 1) # z: (#batch, #latent_size+3, #vertex)
+      y = F.relu(self.bn1(self.conv1(y)))
+      y = F.relu(self.bn2(self.conv2(y)))
+      y = F.relu(self.bn3(self.conv3(y)))
+      y = self.th(self.conv4(y))
 
-      x = torch.cat((x, grid), 1)
-      x = F.relu(self.bn2(self.conv2(x)))
+			# y: (#batch, 3, #vertex)
+      return y
 
-      x = torch.cat((x, grid), 1)
-      x = F.relu(self.bn3(self.conv3(x)))
 
-      x = torch.cat((x, grid), 1)
-      x = self.th(self.conv4(x))
-      return x
+class PointGenSkipconnectedDecoder(nn.Module):
+	def __init__(self, latent_size):
+    
+    self.latent_size = latent_size
+    super(PointGenSkipconnectedDecoder, self).__init__()
+    self.conv1 = torch.nn.Conv1d((self.latent_size//1)+3, self.latent_size//1, kernel_size=1)
+    self.conv2 = torch.nn.Conv1d((self.latent_size//1)+3, self.latent_size//2, kernel_size=1)
+    self.conv3 = torch.nn.Conv1d((self.latent_size//2)+3, self.latent_size//4, kernel_size=1)
+    self.conv4 = torch.nn.Conv1d((self.latent_size//4)+3, 3, kernel_size=1)
+    
+    self.th = nn.Tanh()
+    self.bn1 = torch.nn.BatchNorm1d(self.latent_size//1)
+    self.bn2 = torch.nn.BatchNorm1d(self.latent_size//2)
+    self.bn3 = torch.nn.BatchNorm1d(self.latent_size//4)
+    
+  def forward(self, z, verts):
+      # z     : (#batch, #latent_size)
+      # verts : (#batch, 3, #vertex)
+
+			num_verts = verts.size(2)
+      z = z[:,:,None].repeat(1,1,num_verts) # z: (#batch, #latent_size, #vertex)
+
+      y = torch.cat((z, verts), 1) # y: (#batch, #latent_size+3, #vertex)
+      y = F.relu(self.bn1(self.conv1(y)))
+
+      y = torch.cat((y, verts), 1)
+      y = F.relu(self.bn2(self.conv2(y)))
+
+      y = torch.cat((y, verts), 1)
+      y = F.relu(self.bn3(self.conv3(y)))
+
+      y = torch.cat((y, verts), 1)
+      y = self.th(self.conv4(y))
+
+			# y: (#batch, 3, #vertex)
+      return y
+
 
 class AtlasNetSingle(nn.Module):
   def __init__(self,
@@ -90,8 +90,8 @@ class AtlasNetSingle(nn.Module):
     self.use_pretrained_encoder = use_pretrained_encoder
 		self.use_skipconnected_decoder = use_skipconnected_decoder
 
-    self.encoder = models.resnet18(pretrained=self.use_pretrained_encoder, num_classes=self.latent_size)  
-    #self.decoder = PointGenCon_SkipConection(bottleneck_size=self.bottleneck_size)
+    self.encoder = models.resnet18(pretrained=self.use_pretrained_encoder, num_classes=self.latent_size)
+		self.decoder = PointGenSkipconnectedDecoder(latent_size=self.latent_size) if self.use_skipconnected_decoder else PointGenDecoder(latent_size=self.latent_size)
     
   def forward(self, im, verts):
 		assert len(im.size()) == 4,    'im:    (#batch, 3(rgb), #hight, #width)'
@@ -100,8 +100,6 @@ class AtlasNetSingle(nn.Module):
     im = im[:,:3,:,:] # remove alpha
     z = self.encoder(im) # z: (#batch, latent_size)
     
-    
-    
-    y = self.decoder(z, grid)
+    y = self.decoder(z, verts) # y: (#batch, 3, #vertex)
 
-    return y.transpose(2,1).contiguous()
+    return y
