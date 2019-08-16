9260d605f131b586c42ac5fc9d3bd852fc4a7db0
Yoshihiro Fukuhara <gatheluck+tech@gmail.com>
2019-08-16 10:04:15 +0900

    update train

--- train.py
+++ train.py
@@ -1,6 +1,6 @@
 from dataset import *
 from options import *
-from model import *
+from atlasnet import *
 from utils import *
 
 import os
@@ -23,7 +23,9 @@ if __name__ == '__main__':
 	len_dataset = len(dataset)
 	print('training set: ', len_dataset)
 
-	model = SVR_AtlasNet_SPHERE()
+	model = AtlasNetSingle(latent_size = 1024, 
+												use_pretrained_encoder = opt.pretrained_enc,
+												use_skipconnected_decoder = opt.skipconnected_dec)
 	model.apply(weights_init)
 	model = model.to(opt.device)
 
@@ -51,19 +53,23 @@ if __name__ == '__main__':
 			optimizer.zero_grad()   
 			img, points, label, _ , _= data
 			img, points = img.to(opt.device), points.to(opt.device)
+			# points: (#batch, #verts, 3(xyz))
+			# print("point: ", points.shape)
 
 			# create random grid
-			rand_grid = torch.FloatTensor(img.size(0),3,opt.num_points) #sample points randomly
+
+			rand_grid = torch.FloatTensor(img.size(0), 3, opt.num_points) #sample points randomly
 			rand_grid.normal_(mean=0,std=1)
 			rand_grid = rand_grid / torch.sqrt(torch.sum(rand_grid**2, dim=1, keepdim=True)).expand(img.size(0),3,opt.num_points)
 			rand_grid = rand_grid.to(opt.device)
 
 			# forward
-			points_reconstructed  = model(img, rand_grid)
-			# print(points_reconstructed.cpu().shape) # torch.Size([32, 2500, 3])
-			# print(points.transpose(2,1).contiguous().cpu().shape) # torch.Size([32, 2500, 3])
+			points_reconstructed  = model(img, rand_grid) + rand_grid # (#batch, 3(xyz), #vertex)
+			
+			#print(points_reconstructed.transpose(1,2).cpu().shape) # torch.Size([32, 2500, 3])
+			#print(points.contiguous().cpu().shape) # torch.Size([32, 2500, 3])
 
-			dist1, dist2 = distChamfer(points, points_reconstructed, opt.cuda)
+			dist1, dist2 = distChamfer(points, points_reconstructed.transpose(1,2).contiguous(), opt.cuda)
 
 			loss_net = (torch.mean(dist1)) + (torch.mean(dist2))
 			loss_net.backward()
@@ -75,7 +81,7 @@ if __name__ == '__main__':
 										win = 'TRAIN_INPUT',
 										opts = dict(title="TRAIN_INPUT", markersize=2)
 				)
-				vis.scatter(X = points_reconstructed[0].data.cpu(),
+				vis.scatter(X = points_reconstructed[0].transpose(0,1).data.cpu(),
 										win = 'TRAIN_INPUT_RECONSTRUCTED',
 										opts = dict(title="TRAIN_INPUT_RECONSTRUCTED", markersize=2)
 				)
@@ -96,5 +102,3 @@ if __name__ == '__main__':
 
 	save_model(model, os.path.join(opt.log_dir, 'weight_final.pth'))
 					
-
-
